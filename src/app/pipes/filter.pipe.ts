764eefcd492abb6662970dc38e917661208295ba
ISProdanov <iprodanov666@abv.bg>
2019-09-13 17:40:14 +0300

    Added Resolver & Filters Fix

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -8,12 +8,6 @@ export class FilterPipe implements PipeTransform {
   transform(dataSource: any, position: any, department: any): any {
     if( position === undefined && department === undefined ) return dataSource.data;
 
-    // if (position && department) {
-    //   return dataSource.data.filter(user => {
-    //     return user.positionId === position && user.departmentId === department
-    //   })
-    // }
-
     switch (position || department) {
       case !position && department: {
         return dataSource.data.filter(user => {
